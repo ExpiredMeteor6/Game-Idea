a20e2704379451700149eba391a3d0d4fff81632
Cameron Gibbons <34195037+ExpiredMeteor6@users.noreply.github.com>
2022-05-04 21:36:31 +0100

    changing from looking for colour to looking for block
    allow movement detection

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from animation import Idle
 import sys
 from render import Render
 import time
+import math
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -66,9 +67,47 @@ MOVING_UP = False
 ALLOW_RIGHT = False
 ALLOW_LEFT = False
 ON_GROUND = False
-VERTICAL_MOMENTUM = 0
+VERTICAL_MOMENTUM = 4
 
-player_location = [1024, 100]
+#player_location = [1024, 100]
+player_location = [50, 100]
+
+def screen_to_block_coords(pos):
+    current_block_size = render.BLOCK_SIZE
+    #WHEN CAMERA MOVEMENT IMPLEMENTED USE THIS
+    #horizontal_offset = (render.movement_horizontal * current_block_size) + render.offset_x
+    #horizontal_offset = render.offset_x
+
+    horizontal_offset = 0
+    vertical_offset = 0
+
+    actual_x_pos = math.floor((pos[0] - horizontal_offset) / current_block_size)
+    actual_y_pos = math.floor((pos[1] - vertical_offset) / current_block_size)
+
+    return (actual_x_pos, actual_y_pos)
+
+def get_block(pos):
+    current_block_size = render.BLOCK_SIZE
+    #WHEN CAMERA MOVEMENT IMPLEMENTED USE THIS
+    #horizontal_offset = (render.movement_horizontal * current_block_size) + render.offset_x
+    #horizontal_offset = render.offset_x
+
+    horizontal_offset = 0
+    vertical_offset = 0
+
+    actual_x_pos = math.floor((pos[0] - horizontal_offset) / current_block_size)
+    actual_y_pos = math.floor((pos[1] - vertical_offset) / current_block_size)
+    chunk_coords = (math.floor(actual_x_pos / 8), math.floor(actual_y_pos / 8))
+    block_within_chunk_coords = (actual_x_pos - chunk_coords[0] * 8, actual_y_pos - chunk_coords[1] * 8)
+
+    block = render.TEST_MAP[chunk_coords[0] * 4 + chunk_coords[1]].CHUNK[block_within_chunk_coords[1]][block_within_chunk_coords[0]]
+    return block
+
+def block_to_screen_coords(x, y):
+    #screen_coords = ((x * render.BLOCK_SIZE) + render.offset_x, (y * render.BLOCK_SIZE) + render.offset_y)
+    screen_coords = ((x * render.BLOCK_SIZE) + 0, (y * render.BLOCK_SIZE) + 0)
+    return screen_coords
+
 
 def allow_right():
     if render.screen.get_at((int(player_location[0])+32, int(player_location[1])+30)) != (140, 255, 251, 255):
@@ -82,6 +121,31 @@ def allow_left():
         return False
     else:
         return True
+
+def allow_down(blob_x, blob_y, vertical_momentum):
+    '''
+    currentx,currenty = screen_to_block_coords(blob_screen_position_x, blob_screen_position_y)
+
+    downblockx,downblocky = (current_x,currenty-1)
+    downblockxscreen,downblockyscreen = block_to_screen_coords(downblockx, downblocky)
+
+    vert_distance_to_down = downblockyscreen- blob_screen_position_y
+    '''
+    current_blob_position = screen_to_block_coords((blob_x, blob_y))
+    block_beneath = (current_blob_position[0], current_blob_position[1] - 1)
+    block_beneath_screen_coords = block_to_screen_coords(block_beneath[0], block_beneath[1])
+    print(block_beneath_screen_coords)
+    num_block_beneath = get_block(block_beneath_screen_coords)
+    vertical_distance_to_down = current_blob_position[1] - block_beneath_screen_coords[1]
+
+    #print(num_block_beneath)
+    if vertical_distance_to_down < vertical_momentum and num_block_beneath != 0:
+        return False
+    else:
+        return True
+
+
+
 
 
 last_direction = 'right'
@@ -115,7 +179,8 @@ while running:
             render.screen.blit(render.player_img_left, player_location)
         else:
             render.screen.blit(render.player_img_down_left, player_location)
-    
+
+    '''
     if render.screen.get_at((int(player_location[0]), int(player_location[1])+32 + int(VERTICAL_MOMENTUM + 0.1))) != (140, 255, 251, 255) or render.screen.get_at((int(player_location[0]+26), int(player_location[1])+32 + int(VERTICAL_MOMENTUM + 0.1))) != (140, 255, 251, 255):
         VERTICAL_MOMENTUM = 0
         ON_GROUND = True
@@ -123,8 +188,17 @@ while running:
         ON_GROUND = False
         VERTICAL_MOMENTUM += 0.1
         player_location[1] += VERTICAL_MOMENTUM
-    
-    
+    '''
+    if allow_down(int(player_location[0]), int(player_location[1]) + render.BLOCK_SIZE * 2, VERTICAL_MOMENTUM) == True:
+        #print("allowed down")
+        ON_GROUND = False
+        #VERTICAL_MOMENTUM += 0.1
+        player_location[1] += VERTICAL_MOMENTUM
+    else:
+        #print("not allowed down")    
+        VERTICAL_MOMENTUM = 4
+        ON_GROUND = True
+
     if MOVING_RIGHT == True and allow_right() == True:
         player_location[0] += 4
         last_direction = 'right'
