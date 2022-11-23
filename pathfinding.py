a59a927345f88f02c8f4ac4ab90c4b0c74ac508c
Cameron Gibbons <34195037+ExpiredMeteor6@users.noreply.github.com>
2022-11-23 11:42:09 +0000

    ghgh

--- pathfinding.py
+++ pathfinding.py
@@ -12,28 +12,17 @@ class ConnectionAssessor():
     def convert_pos_to_block_numbers(self, pos):
         chunk_x = pos[0] // 8
         chunk_y = pos[1] // 8
-        if chunk_x > 0:
-            chunk_x = chunk_x * 4
-        
-        chunk = chunk_x + chunk_y
-
         block_x = pos[0] % 8
         block_y = pos[1] % 8
-
-        row = block_y
-
-        block = block_x
-
-
-
-        return chunk, row, block
+        return chunk_x, chunk_y, block_x, block_y
     
     def get_connected_nodes(self, pos):
         #Left One Block
-        blockcoordsleft = self.convert_pos_to_block_numbers((pos[0]-1, pos[1]))
-        print(blockcoordsleft)
+        blockcoords = self.convert_pos_to_block_numbers((pos[0]-1, pos[1]))
+        
+        print(blockcoords)
         print(self.render.LEVEL_MAP_NUMBERS)
-        if self.render.LEVEL_MAP_NUMBERS[blockcoordsleft[0]][blockcoordsleft[1]][blockcoordsleft[2]] in self.traversable_blocks:
+        if self.render.LEVEL_MAP_NUMBERS[blockcoords[0]*4+blockcoords[1]][blockcoords[2]][blockcoords[3]] in self.traversable_blocks:
             return self.render.LEVEL_MAP_NUMBERS[blockcoordsleft]
         return pos
 
