9a1835f89d07c54a99779a1b61c783c3134d5756
Cameron Gibbons <34195037+ExpiredMeteor6@users.noreply.github.com>
2022-11-23 11:58:33 +0000

    left connected thingy working (monkey code 🙈)
    yes

--- pathfinding.py
+++ pathfinding.py
@@ -1,5 +1,6 @@
 import pygame
 from render import Render
+from file_handler import File_Handler
 
 class PathFinder():
     pass
@@ -7,23 +8,27 @@ class PathFinder():
 class ConnectionAssessor():
     def __init__(self):
         self.render = Render()
+        self.MAP = File_Handler().load()
         self.traversable_blocks = [0]
-    
+   
     def convert_pos_to_block_numbers(self, pos):
         chunk_x = pos[0] // 8
         chunk_y = pos[1] // 8
         block_x = pos[0] % 8
         block_y = pos[1] % 8
         return chunk_x, chunk_y, block_x, block_y
-    
+   
     def get_connected_nodes(self, pos):
         #Left One Block
-        blockcoords = self.convert_pos_to_block_numbers((pos[0]-1, pos[1]))
-        
-        print(blockcoords)
-        print(self.render.LEVEL_MAP_NUMBERS)
-        if self.render.LEVEL_MAP_NUMBERS[blockcoords[0]*4+blockcoords[1]][blockcoords[2]][blockcoords[3]] in self.traversable_blocks:
-            return self.render.LEVEL_MAP_NUMBERS[blockcoordsleft]
+        chunk_x,chunk_y,block_x,block_y = self.convert_pos_to_block_numbers((pos[0]-1, pos[1]))
+       
+        print(chunk_x,chunk_y,block_x,block_y)
+        chunk = self.MAP[chunk_x*4+chunk_y]
+        row = chunk[block_y]
+        block = row[block_x]
+
+        if block in self.traversable_blocks:
+            return block
         return pos
 
 class Node():
