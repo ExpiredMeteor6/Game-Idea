0edf9067e4480d864da2085edcc6afdef96b633e
ExpiredMeteor6 <34195037+ExpiredMeteor6@users.noreply.github.com>
2022-12-07 12:09:55 +0000

    bugs

--- render.py
+++ render.py
@@ -2,6 +2,7 @@ import pygame
 from terrain_handler import Chunk
 from file_handler import File_Handler
 import math
+from PIL import Image
 
 class Render:
     def __init__(self):
@@ -92,7 +93,7 @@ class Render:
         self.screen = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
         self.BG = pygame.image.load('Images/Background.png').convert()
 
-        self.grass_img = pygame.image.load('Images/grass.png').convert()
+        '''self.grass_img = pygame.image.load('Images/grass.png').convert()
         self.air_img = pygame.image.load('Images/air.png').convert()
         self.dirt_img = pygame.image.load('Images/dirt.png').convert()
         self.stone_img = pygame.image.load('Images/stone.png').convert()
@@ -108,7 +109,9 @@ class Render:
         self.gravel_img = pygame.transform.scale(self.gravel_img, (self.BLOCK_SIZE, self.BLOCK_SIZE))
         self.iron_ore_img = pygame.transform.scale(self.iron_ore_img, (self.BLOCK_SIZE, self.BLOCK_SIZE))
         self.start_img = pygame.transform.scale(self.start_img, (self.BLOCK_SIZE, self.BLOCK_SIZE))
-        self.testing_img = pygame.transform.scale(self.testing_img, (self.BLOCK_SIZE, self.BLOCK_SIZE))
+        self.testing_img = pygame.transform.scale(self.testing_img, (self.BLOCK_SIZE, self.BLOCK_SIZE))'''
+
+        self.grass_img = Image.load ??????
 
         pygame.mixer.init()
         self.music = pygame.mixer.music
@@ -153,7 +156,63 @@ class Render:
             self.LEVEL_MAP.append(current_chunk)
             y += 1
 
+    def convert_pil_image_to_pygame(self, img):
+        return pygame.image.fromstring(img.tobytes(), img.size, img.mode)
+
+    def convert_pygame_image_to_pil(self, img):
+        return Image.frombytes("RGBA",(self.BLOCK_SIZE,self.BLOCK_SIZE),pygame.image.tostring(img, "RGBA", False))
+    
+    def draw_chunk(self, chunk_x, chunk_y):
+        chunk = self.LEVEL_MAP[chunk_x*4+chunk_y]
+        image = None 
+        if image is None:
+            new_image = Image.new('RGB',(8 * self.BLOCK_SIZE ,8 * self.BLOCK_SIZE), (250,250,250))
+            y = 0
+            for row in chunk.CHUNK:
+                for x in range(len(row)):
+                    placex = (x) * self.BLOCK_SIZE
+                    placey = (y) * self.BLOCK_SIZE
+
+                    block = row[x]
+
+                    place_img = lambda i : new_image.paste(self.convert_pygame_image_to_pil(i),(placex,placey))
+                    place_rotated_img = lambda i : place_img(self.place_rotated_block(i, y, x))
+
+                    if block == 0:
+                        place_img(self.air_img)
+
+                    elif block == 1:
+                        place_img(self.grass_img)
+
+                    elif block == 2:
+                        place_rotated_img(self.dirt_img)
+                    
+                    elif block == 3:
+                        place_rotated_img(self.stone_img)
+                    
+                    elif block == 4:
+                        place_rotated_img(self.iron_ore_img)
+                    
+                    elif block == 5:
+                        place_img(self.start_img)
+                        self.start_coords = (x, y)
+                    
+                    elif block == 6:
+                        place_img(self.testing_img)
+
+                y += 1
+            '''new_image.show()'''
+
+        self.screen.blit(self.convert_pil_image_to_pygame(new_image), (chunk_x * 8 * self.BLOCK_SIZE, chunk_y * 8 * self.BLOCK_SIZE))
+
     def draw_level(self):
+        for chunk in self.LEVEL_MAP:
+            x = chunk.x_y[0]
+            y = chunk.x_y[1]
+
+            self.draw_chunk(x, y)
+
+        '''
         for chunk in self.LEVEL_MAP:
             y = 0
             for row in chunk.CHUNK:
@@ -193,6 +252,7 @@ class Render:
 
                     x += 1
                 y += 1
+                '''
 
     def wipe(self):
         self.screen.fill((0,0,0))
@@ -211,4 +271,3 @@ class Render:
             current_chunk.x_y = (x, y)
             self.LEVEL_MAP.append(current_chunk)
             y += 1
-    
